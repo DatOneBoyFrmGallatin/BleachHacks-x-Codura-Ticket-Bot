120c148869f08789892a424b91256390727e079d
𝚈𝙶 𝚂𝚖𝚘𝚔𝚊😮‍💨 <b4staysnapping@gmail.com>
2025-11-17 11:28:42 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  TOKEN: process.env.TOKEN || '',
-  CLIENT_ID: process.env.CLIENT_ID || '',
-  GUILD_ID: process.env.GUILD_ID || '',
-  // Optional: role allowed to automatically see/claim tickets (set this as a role ID in Railway variables)
-  ADMIN_ROLE_ID: process.env.ADMIN_ROLE_ID || ''
+    TOKEN: process.env.TOKEN,
+    CLIENT_ID: process.env.CLIENT_ID,
+    GUILD_ID: process.env.GUILD_ID
 };
